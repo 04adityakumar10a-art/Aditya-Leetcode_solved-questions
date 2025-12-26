e5a73a63b051ed562e4558b44cb8f99a34bf1492
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2025-12-26 14:24:35 +0530

    Time: 2 ms (99.79%), Space: 44.7 MB (98.15%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,5 +1,39 @@
 class Solution {
     public String minWindow(String s, String t) {
         
+        
+        int[] map = new int[128]; // ASCII mapping for faster access
+        int charsToCover = t.length();
+        
+        for (char c : t.toCharArray()) {
+            map[c]++;
+        }
+        
+        int left = 0, minLeft = 0, minLen = Integer.MAX_VALUE;
+        
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            
+            if (map[rightChar] > 0) {
+                charsToCover--;
+            }
+            map[rightChar]--;
+            
+            while (charsToCover == 0) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    minLeft = left;
+                }
+                
+                char leftChar = s.charAt(left);
+                map[leftChar]++;
+                if (map[leftChar] > 0) {
+                    charsToCover++;
+                }
+                left++;
+            }
+        }
+        
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLen);
     }
 }

afa09f17b766a005b96b4eba0f7106e6874a214f
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-01-01 22:32:33 +0530

    Time: 2 ms (59.16%), Space: 46.7 MB (9.52%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,31 +15,33 @@
  */
 
 class Solution {
-   int postIndex ;
-    HashMap<Integer, Integer> map = new HashMap<>();
+    int postIndex;
+    Map<Integer, Integer> inorderMap = new HashMap<>();
 
-    public TreeNode buildTree(int[] inorder, int[] postorder) {
-         postIndex = postorder.length-1; 
-        // Store inorder positions
-        for (int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
+    public TreeNode buildTree(int[] inorder, int[] postorder) 
+    {
+        postIndex = postorder.length - 1;
+
+        for (int i = 0; i < inorder.length; i++) 
+        {
+            inorderMap.put(inorder[i], i);
         }
-        return construct(postorder, 0, inorder.length - 1);
+
+        return build(inorder, postorder, 0, inorder.length - 1);
     }
 
-    private TreeNode construct(int[] postorder, int left, int right) {
-       if (left > right || postIndex < 0) return null;
+    private TreeNode build(int[] inorder, int[] postorder, int left, int right) 
+    {
+        if (left > right) return null;
+
         int rootVal = postorder[postIndex--];
-        int mid = map.get(rootVal);
         TreeNode root = new TreeNode(rootVal);
 
-        // Build left and right subtree
-      
-        root.right = construct(postorder, mid + 1, right);
-        root.left = construct(postorder, left, mid - 1);
-        // Root from preorder
-       
-       
+        int index = inorderMap.get(rootVal);
+
+        root.right = build(inorder, postorder, index + 1, right);
+        root.left = build(inorder, postorder, left, index - 1);
+
         return root;
     }
 }

1208604418addbcee32b58fdca468d019cfc0b6d
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-01-06 21:48:13 +0530

    Time: 0 ms (100%), Space: 44.8 MB (83.57%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,26 +14,12 @@
  * }
  */
 class Solution {
-    public boolean isleafnode(TreeNode node)
-    {if(node.left == null && node.right==null) return true ;
-    return false ;}
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null) return false;
-        Queue<TreeNode> q = new LinkedList<>();
-         Queue<Integer> q1 = new LinkedList<>();
-         q.add(root);  
-         q1.add(root.val);                        
-         while(!q.isEmpty())
-         { int size=q.size();
-          for(int i=0;i<size;i++)
-         {
-             TreeNode node=q.poll();
-             int nodeval = q1.poll();
-             if( nodeval==targetSum && isleafnode(node)) return true;
-             if(node.left!=null){q.add(node.left);q1.add(node.left.val+nodeval);}
-             if(node.right!=null){q.add(node.right);q1.add(node.right.val+nodeval);}
-              }
-             }
-        return false;
+        targetSum=targetSum-root.val;
+        if(root.left==null && root.right==null){
+            return targetSum==0;
+        }
+        return hasPathSum(root.left,targetSum) || hasPathSum(root.right,targetSum);
     }
 }

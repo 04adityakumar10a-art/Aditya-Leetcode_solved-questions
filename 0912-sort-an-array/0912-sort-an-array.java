1da5197f3d0112649ddf8efa88fe3bf03123b558
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-01-13 00:09:07 +0530

    Time: 98 ms (17.1%), Space: 81.3 MB (81.17%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,6 +1,15 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        PriorityQueue<Integer> minheap = new PriorityQueue<>();
+        for(int num : nums)
+        {
+            minheap.add(num);
+        }
+        int i=0;
+        while(!minheap.isEmpty())
+        {
+            nums[i++]=minheap.poll();
+        }
         return nums;
     }
 }

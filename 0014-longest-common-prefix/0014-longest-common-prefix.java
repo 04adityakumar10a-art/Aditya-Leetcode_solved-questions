a810dd00059415d21c5a2db8bb56aff6e1594a14
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-01-14 22:45:54 +0530

    Time: 1 ms (64.8%), Space: 43.9 MB (5.08%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,15 +1,47 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
 
-        String prefix = strs[0];
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd = false;
+        int childCount = 0;
+    }
 
-        for (int i = 1; i < strs.length; i++) {
-            while (!strs[i].startsWith(prefix)) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.length() == 0) return "";
+    void insert(TrieNode root, String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new TrieNode();
+                curr.childCount++;
+            }
+            curr = curr.children[idx];
+        }
+        curr.isEnd = true;
+    }
+
+    String getLCP(TrieNode root) {
+        StringBuilder sb = new StringBuilder();
+        TrieNode curr = root;
+
+        while (curr.childCount == 1 && !curr.isEnd) {
+            for (int i = 0; i < 26; i++) {
+                if (curr.children[i] != null) {
+                    sb.append((char)(i + 'a'));
+                    curr = curr.children[i];
+                    break;
+                }
             }
         }
-        return prefix;
+        return sb.toString();
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        TrieNode root = new TrieNode();
+
+        for (String s : strs) {
+            insert(root, s);
+        }
+
+        return getLCP(root);
     }
 }

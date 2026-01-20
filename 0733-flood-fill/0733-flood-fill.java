83468739a7318980af9922d3863748aad34bfed2
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-01-20 22:30:36 +0530

    Time: 114 ms (1.55%), Space: 47 MB (32.6%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,41 +1,28 @@
 class Solution {
-    static class Pair {
-        int row, col;
-        Pair(int i, int j) {
-            this.row = i;
-            this.col = j;
-        }
-    }
-    public static void bfs(int[][] image, int sr, int sc, int orgclr,int color)
-    { Stack<Pair> st= new Stack<>();
-     int m = image.length;
-     int n = image[0].length;
-      st.push(new  Pair(sr,sc));
-      while(!st.isEmpty())
-      {  Pair val= st.pop(); 
-         int i= val.row;
-         int j = val.col;
-         if(image[i][j]==orgclr)
-         {  
-           // update
-            image[i][j]=color;
-            //left
-            if(j>0) st.push(new Pair(i,j-1));
-           //right
-            if(j<n-1) st.push(new Pair(i,j+1));
-           //up
-            if(i>0) st.push(new Pair(i-1,j));
-           //down
-           if(i<m-1)  st.push(new Pair(i+1,j));
 
-           
-         }
-      }
+    public void dfs(int[][] image, int i, int j, int orgclr, int color) {
+
+        int m = image.length;
+        int n = image[0].length;
+
+        // boundary + color check
+        if (i < 0 || j < 0 || i >= m || j >= n) return;
+        if (image[i][j] != orgclr) return;
+
+        // mark visited
+        image[i][j] = color;
+
+        // explore neighbors
+        dfs(image, i, j - 1, orgclr, color); // left
+        dfs(image, i, j + 1, orgclr, color); // right
+        dfs(image, i - 1, j, orgclr, color); // up
+        dfs(image, i + 1, j, orgclr, color); // down
     }
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int orgclr= image[sr][sc];
-        if (orgclr == color) return image;
-        bfs(image ,sr ,sc,orgclr,color);
+        int orgclr = image[sr][sc];
+        if (orgclr == color) return image; // prevent infinite recursion
+        dfs(image, sr, sc, orgclr, color);
         return image;
     }
 }

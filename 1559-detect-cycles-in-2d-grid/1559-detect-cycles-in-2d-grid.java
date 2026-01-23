7e54e802642b6d6a4334f7e0e373b0cbee2554ff
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-01-23 16:28:09 +0530

    Time: 13 ms (97.57%), Space: 137.9 MB (67.19%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,14 +1,4 @@
 class Solution {
-    static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
-                writer.write("0");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }));
-    }
     public static boolean containsCycle(char[][] grid) {
         boolean[][] visited = new boolean[grid.length][grid[0].length];
 
@@ -26,25 +16,29 @@ class Solution {
     static boolean dfs(char[][] grid, boolean[][] visited, int row, int col, int prow, int pcol) {
         char ch = grid[row][col];
         visited[row][col] = true;
-
+       //up
         if(row > 0 && grid[row - 1][col] == ch) {
             if(!visited[row - 1][col]) {
                 if(dfs(grid, visited, row - 1, col, row, col)) return true;
             }
             else if(row - 1 != prow || col != pcol) return true;
         }
-        if(col > 0 && grid[row][col - 1] == ch) {
-            if(!visited[row][col - 1]) {
-                if(dfs(grid, visited, row, col - 1, row, col)) return true;
-            }
-            else if(row != prow || col - 1 != pcol) return true;
-        }
+         //down
         if(row < grid.length - 1 && grid[row + 1][col] == ch) {
             if(!visited[row + 1][col]) {
                 if(dfs(grid, visited, row + 1, col, row, col)) return true;
             }
             else if(row + 1 != prow || col != pcol) return true;
         }
+        //left
+        if(col > 0 && grid[row][col - 1] == ch) {
+            if(!visited[row][col - 1]) {
+                if(dfs(grid, visited, row, col - 1, row, col)) return true;
+            }
+            else if(row != prow || col - 1 != pcol) return true;
+        }
+       
+        //right
         if(col < grid[0].length - 1 && grid[row][col + 1] == ch) {
             if(!visited[row][col + 1]) {
                 if(dfs(grid, visited, row, col + 1, row, col)) return true;

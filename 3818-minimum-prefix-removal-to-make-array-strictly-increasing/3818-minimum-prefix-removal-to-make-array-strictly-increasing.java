e8b219000afe79c75f7651a1b045c6517434c3b3
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-01-25 23:11:32 +0530

    Time: 1 ms (99.26%), Space: 120.5 MB (22.54%) - LeetHub

--- 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.java
+++ 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.java
@@ -1,9 +1,10 @@
 class Solution {
     public int minimumPrefixLength(int[] nums) {
         int pause=0;
-        for(int i=0 ; i<nums.length-1 ; i++)
+        for(int i=nums.length-1 ; i>0 ; i--)
         {
-            if(nums[i]>=nums[i+1] ) pause=i+1;
+            if(nums[i]<=nums[i-1] ) 
+            {pause=i; break;}
         }
         return pause;
     }

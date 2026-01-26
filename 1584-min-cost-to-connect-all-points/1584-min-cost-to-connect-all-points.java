29ce1b66669ee550780e870b7b044d8774319f59
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-01-26 23:40:57 +0530

    Time: 656 ms (13.24%), Space: 164.4 MB (14.74%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Solution {
+    static class Pair {
+        int node;
+        int weight;
+
+        Pair(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
+    }
+
+    public static int primsMST(int V, ArrayList<ArrayList<Pair>> adj) {
+
+        boolean[] visited = new boolean[V];
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
+                (a, b) -> a.weight - b.weight);
+
+        pq.add(new Pair(0, 0));
+        int mstWeight = 0;
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.poll();
+
+            int node = curr.node;
+            int weight = curr.weight;
+
+            if (visited[node]) continue;
+
+            visited[node] = true;
+            mstWeight += weight;
+
+            for (Pair nbr : adj.get(node)) {
+                if (!visited[nbr.node]) {
+                    pq.add(new Pair(nbr.node, nbr.weight));
+                }
+            }
+        }
+
+        return mstWeight;
+    }
+
+    public int minCostConnectPoints(int[][] points) {
+
+        int n = points.length;
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+
+                int distance = Math.abs(points[i][0] - points[j][0])
+                             + Math.abs(points[i][1] - points[j][1]);
+
+                adj.get(i).add(new Pair(j, distance));
+            }
+        }
+
+        return primsMST(n, adj);
+    }
+}

bcc50721de8f8291d00064f0454cbe4ac460e9b4
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-02-18 23:36:57 +0530

    Time: 5 ms (98.65%), Space: 47 MB (11.56%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,57 +1,32 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
 
-        Map<Integer, List<int[]>> graph = new HashMap<>();
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
 
-        for (int[] f : flights) {
-            graph.putIfAbsent(f[0], new ArrayList<>());
-            graph.get(f[0]).add(new int[]{f[1], f[2]});
-        }
+        // We need K+1 relaxations (K stops = K+1 edges)
+        for (int i = 0; i <= K; i++) {
 
-        // {node, cost, stops}
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> a[1] - b[1]
-        );
+            // Copy previous distances
+            int[] temp = Arrays.copyOf(dist, n);
 
-        pq.offer(new int[]{src, 0, 0});
+            for (int[] flight : flights) {
+                int u = flight[0];
+                int v = flight[1];
+                int price = flight[2];
 
-        // distance[node][stops] = min cost
-        int[][] dist = new int[n][K + 2];
-
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-
-        dist[src][0] = 0;
-
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-
-            int node = curr[0];
-            int cost = curr[1];
-            int stops = curr[2];
-
-            if (node == dst)
-                return cost;
-
-            if (stops <= K) {
-                if (!graph.containsKey(node))
+                if (dist[u] == Integer.MAX_VALUE)
                     continue;
 
-                for (int[] neighbor : graph.get(node)) {
-                    int nextNode = neighbor[0];
-                    int price = neighbor[1];
-
-                    int newCost = cost + price;
-
-                    if (newCost < dist[nextNode][stops + 1]) {
-                        dist[nextNode][stops + 1] = newCost;
-                        pq.offer(new int[]{nextNode, newCost, stops + 1});
-                    }
+                if (dist[u] + price < temp[v]) {
+                    temp[v] = dist[u] + price;
                 }
             }
+
+            dist = temp;
         }
 
-        return -1;
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
     }
 }

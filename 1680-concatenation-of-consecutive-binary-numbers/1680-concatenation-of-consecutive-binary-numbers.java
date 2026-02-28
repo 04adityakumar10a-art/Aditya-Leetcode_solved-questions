401f59b110edbcc76ab700bc790b4c7a952fd47c
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-02-28 14:29:56 +0530

    Time: 616 ms (19.77%), Space: 103.9 MB (6.98%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -1,12 +1,22 @@
 class Solution {
     public int concatenatedBinary(int n) {
-        final long modulo = (long) (1e9 + 7);
-        long result = 0;
-        int binaryDigits = 0;
-        for (int i = 1; i <= n; i++) {
-            if ((i & (i - 1)) == 0) binaryDigits++;
-            result = ((result << binaryDigits) + i) % modulo;
+        final long MOD = 1000000007;
+        
+        // Step 1: Build concatenated binary string
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 1; i <= n; i++) {
+            sb.append(Integer.toBinaryString(i));
         }
-        return (int) result;
+        
+        // Step 2: Convert binary string to decimal with modulo
+        long ans = 0;
+        String s = sb.toString();
+        
+        for(int i = 0; i < s.length(); i++) {
+            ans = (ans * 2 + (s.charAt(i) - '0')) % MOD;
+        }
+        
+        return (int) ans;
     }
 }

0df619876cbdc52c37d0d8da9085866bed7e76fd
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-03-02 23:49:54 +0530

    Time: 7 ms (65.52%), Space: 47.1 MB (19.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,37 @@
 class Solution {
+
+    Integer[][] dp;
+    int totalSum;
+
     public int findTargetSumWays(int[] nums, int target) {
+
+        totalSum = 0;
+        for (int num : nums)
+            totalSum += num;
+
+        if (Math.abs(target) > totalSum)
+            return 0;
+
+        dp = new Integer[nums.length][2 * totalSum + 1];
+
         return rec(nums, target, nums.length - 1);
     }
 
     public int rec(int[] nums, int target, int index) {
 
-        // Base case: no elements left
-        if (index < 0) {
+        // prune impossible targets
+        if (Math.abs(target) > totalSum)
+            return 0;
+
+        if (index < 0)
             return target == 0 ? 1 : 0;
-        }
+
+        if (dp[index][target + totalSum] != null)
+            return dp[index][target + totalSum];
 
         int plus = rec(nums, target - nums[index], index - 1);
         int minus = rec(nums, target + nums[index], index - 1);
 
-        return plus + minus;
+        return dp[index][target + totalSum] = plus + minus;
     }
 }

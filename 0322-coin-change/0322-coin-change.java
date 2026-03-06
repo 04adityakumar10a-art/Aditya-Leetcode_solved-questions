688a64d6a1502f00a1548ecee263075a3f08b290
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-03-07 00:20:53 +0530

    Time: 16 ms (66.12%), Space: 46.8 MB (40.53%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,36 +1,33 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int dp[][] = new int[n][amount+1];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1);
+        int prev[] = new int[amount+1];
+        for(int a=0;a<amount+1;a++){
+            if(a % coins[0]==0){
+                prev[a] = a / coins[0];
+            }else{
+                prev[a] = (int)(1e9);
+            }
         }
-        int res = recur(coins, amount, n-1,dp);
-        if(res == (int)(1e9)){
+
+        for(int i=1;i<n;i++){
+            int cur[] = new int[amount+1];
+            for(int a=0;a<amount+1;a++){
+                int pick = (int)(1e9);
+                if(a >= coins[i]){
+                    pick = 1 + cur[a-coins[i]]; //recur(coins, a - coins[i], i, dp);
+                }
+                int noPick = prev[a]; //recur(coins, a, i-1, dp);
+                cur[a] = Math.min(pick, noPick);
+            }
+            prev = cur;
+        }
+
+        if(prev[amount] == (int)(1e9)){
             return -1;
         }
-        return res;
+        return prev[amount];
     }
-    public int recur(int coins[], int amount, int index, int dp[][]){
-        //base case
-        if(index == 0){
-            if(amount % coins[index]==0){
-                dp[index][amount] = amount / coins[index];
-                return amount / coins[index];
-            }
-            dp[index][amount] = (int)(1e9);
-            return (int)(1e9);
-        }
-        if(dp[index][amount]!=-1){
-            return dp[index][amount];
-        }
-        int pick = (int)(1e9);
-        if(amount >= coins[index]){
-            pick = 1 + recur(coins, amount - coins[index], index, dp);
-        }
-        int noPick = recur(coins, amount, index-1, dp);
-        dp[index][amount] = Math.min(pick, noPick);
-        return dp[index][amount];
-    }
+
 }
 

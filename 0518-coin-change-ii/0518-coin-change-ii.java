2160f679755f80b318a69ebe401dd00e95e1882b
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-03-07 00:35:09 +0530

    Time: 10 ms (67.01%), Space: 46.2 MB (65.68%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int change( int amount,int[] coins) {
+        int n = coins.length;
+        int prev[] = new int[amount+1];
+        for(int a=0;a<amount+1;a++){
+            if(a % coins[0]==0){
+                prev[a] = 1;
+            }else{
+                prev[a] = 0;
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            int cur[] = new int[amount+1];
+            for(int a=0;a<amount+1;a++){
+                int pick =0;
+                if(a >= coins[i]){
+                    pick = cur[a-coins[i]] ; //recur(coins, a - coins[i], i, dp);
+                }
+                int noPick = prev[a]; //recur(coins, a, i-1, dp);
+                cur[a] = pick+ noPick;
+            }
+            prev = cur;
+        }
+        return prev[amount];
+    }
+
+}

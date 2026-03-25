f7a4f4341d4c20efc0ffda90759c556b34fe455b
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-03-25 23:14:18 +0530

    Time: 9 ms (17.48%), Space: 158.7 MB (99.3%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -1,37 +1,49 @@
 class Solution {
     public boolean canPartitionGrid(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
+        int size = m * n;
 
-        long[] rowSum = new long[m];
-        long[] colSum = new long[n];
-        long total = 0;
+        long totalsum = 0;
 
-        // Compute sums
+        long[] prerow = new long[size + 1];
+        long[] precol = new long[size + 1];
+
+        int idx = 1;
+
+        // row-wise flatten
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                rowSum[i] += grid[i][j];
-                colSum[j] += grid[i][j];
-                total += grid[i][j];
+                totalsum += grid[i][j];
+                prerow[idx] = prerow[idx - 1] + grid[i][j];
+                idx++;
             }
         }
 
-        if (total % 2 != 0) return false;
+        idx = 1;
 
-        if (check(rowSum, total)) return true;
-        if (check(colSum, total)) return true;
+        // column-wise flatten
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                precol[idx] = precol[idx - 1] + grid[i][j];
+                idx++;
+            }
+        }
 
-        return false;
-    }
+        if (totalsum % 2 != 0) return false;
 
-    private boolean check(long[] arr, long total) {
-        long left = arr[0];
-        long right = total - left;
+        long target = totalsum / 2;
 
-        for (int i = 1; i < arr.length; i++) {
-            if (left == right) return true;
-            else if (left > right) return false;
-            left += arr[i];
-            right -= arr[i];
+        // row cuts
+        for (int i = 1; i <= size; i++) {
+            if (prerow[i] == target && (i % n == 0) && i != size)
+                return true;
+        }
+
+        // column cuts
+        for (int i = 1; i <= size; i++) {
+            if (precol[i] == target && (i % m == 0) && i != size)
+                return true;
         }
 
         return false;

215abf1375c2ca1feb251a428f6475a1496d9e2f
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-04-21 00:41:44 +0530

    Time: 0 ms (100%), Space: 42.9 MB (97.97%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -1,17 +1,24 @@
 class Solution {
     public int maxDistance(int[] colors) {
-        // lets try 2 pointer ;
-        int max=Integer.MIN_VALUE;
-       for(int i=0; i<colors.length;i++)
-       { 
-        for(int j=colors.length-1;j>=0;j--)
-        {
-            if(colors[i]!=colors[j])
-            {
-              max=Math.max(max,Math.abs(i-j));
+        int n = colors.length;
+        int max = 0;
+
+        // compare with first element
+        for (int j = n - 1; j >= 0; j--) {
+            if (colors[j] != colors[0]) {
+                max = Math.max(max, j - 0);
+                break; // no need to check further
             }
         }
+
+        // compare with last element
+        for (int i = 0; i < n; i++) {
+            if (colors[i] != colors[n - 1]) {
+                max = Math.max(max, (n - 1) - i);
+                break; // no need to check further
+            }
         }
+
         return max;
     }
 }

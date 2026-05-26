31e4cced7bcae620777948f06b8d67312e7549ed
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-05-26 09:53:09 +0530

    Time: 1 ms (100%), Space: 43.6 MB (71.43%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,33 +1,34 @@
 class Solution {
-    public int numberOfSpecialChars(String str) {
-      int count=0;
-      HashMap<Character,Boolean> mp= new HashMap<>();
-        for(int i=0;i<str.length();i++)
-      {
-        mp.put(str.charAt(i),false);
-      }
-      for(int i=0;i<str.length();i++)
-      {
-        if(str.charAt(i)>='a' && str.charAt(i)<='z')
-        { char ch = Character.toUpperCase(str.charAt(i));
-          if(mp.containsKey(ch) && mp.get(str.charAt(i))==false) 
-          {
-             count++;
-             mp.put(str.charAt(i),true);
-             mp.put(Character.toUpperCase(str.charAt(i)),true);
-          }
+
+    public int numberOfSpecialChars(String word) {
+
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
+
+        // Traverse string
+        for (char ch : word.toCharArray()) {
+
+            // lowercase
+            if (Character.isLowerCase(ch)) {
+                lower[ch - 'a'] = true;
+            }
+
+            // uppercase
+            else {
+                upper[ch - 'A'] = true;
+            }
         }
-        else
-           {char ch = Character.toLowerCase(str.charAt(i));
-            if(mp.containsKey(ch) && mp.get(str.charAt(i))==false)
-          {
-             count++;
-             mp.put(str.charAt(i),true);
-             mp.put(Character.toLowerCase(str.charAt(i)),true);
-          }
-           }
-        
-      }
-      return count;
+
+        int count = 0;
+
+        // Check both exist
+        for (int i = 0; i < 26; i++) {
+
+            if (lower[i] && upper[i]) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }

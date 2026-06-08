ce4ceaa62fcf6e52a623b1be2607c708265cd7a8
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-06-08 11:35:57 +0530

    Time: 15 ms (15.65%), Space: 148.9 MB (98.01%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,24 +1,29 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         ArrayList<Integer> small = new ArrayList<>();
-        ArrayList<Integer> pt = new ArrayList<>();
+        ArrayList<Integer> equal = new ArrayList<>();
         ArrayList<Integer> large = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < pivot)
-                small.add(nums[i]);
-          else  if (nums[i] == pivot)
-                pt.add(nums[i]);
-            else
-                large.add(nums[i]);
-        }
-        List<Integer> result = new ArrayList<>();
-        result.addAll(small);
-        result.addAll(pt);
-        result.addAll(large);
-          for (int i = 0; i < nums.length; i++) {
-            nums[i]=result.get(i);
-          }
-        return nums;
 
+        for (int num : nums) {
+            if (num < pivot)
+                small.add(num);
+            else if (num == pivot)
+                equal.add(num);
+            else
+                large.add(num);
+        }
+
+        int idx = 0;
+
+        for (int num : small)
+            nums[idx++] = num;
+
+        for (int num : equal)
+            nums[idx++] = num;
+
+        for (int num : large)
+            nums[idx++] = num;
+
+        return nums;
     }
 }

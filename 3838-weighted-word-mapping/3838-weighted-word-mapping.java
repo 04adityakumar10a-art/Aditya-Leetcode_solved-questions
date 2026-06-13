844e77c43fce34a86741b0fd8f682e44ee693aae
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-06-13 09:51:44 +0530

    Time: 1 ms (100%), Space: 46.6 MB (33.48%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -1,16 +1,18 @@
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
-        String ans="";
-        for(String s : words)
-        {     int sum=0;
-            for(int i=0;i<s.length();i++)
-            { 
-              sum+=weights[s.charAt(i)-'a'];  
+        char[] tempChar = new char[words.length];
+        char[] reverseAlphabets = {'z', 'y', 'x', 'w','v','u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a' };
+        for(int i=0;i<words.length;i++){
+            String word = words[i];
+            int sum = 0;
+            for(int j=0;j<word.length();j++){
+                char key = word.charAt(j);
+                sum += weights[key-97];
             }
-              sum=sum%26;
-              char a =(char)(122-sum);
-              ans=ans+a;
+            sum = sum % 26;
+            tempChar[i] = reverseAlphabets[sum];
         }
-        return ans;
+
+        return new String(tempChar);
     }
 }

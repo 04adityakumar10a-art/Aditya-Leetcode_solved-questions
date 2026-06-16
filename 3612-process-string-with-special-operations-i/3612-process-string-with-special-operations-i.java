9c629815c89d8671f75308dcab58f42f1e322b56
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-06-16 10:16:31 +0530

    Time: 4 ms (76.47%), Space: 55.2 MB (58.82%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public String processStr(String s) {
-        StringBuffer sb = new StringBuffer();
+         StringBuilder sb = new StringBuilder();
         for(int i=0;i<s.length();i++)
         { 
             if(s.charAt(i)>='a' && s.charAt(i)<='z')

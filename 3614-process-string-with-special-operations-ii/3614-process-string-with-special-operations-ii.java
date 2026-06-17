df4db254dce8144be0eb893b19e97374f33437dc
04adityakumar10a-art <04.adityakumar.10a@gmail.com>
2026-06-17 09:58:45 +0530

    Time: 28 ms (88.46%), Space: 48 MB (42.31%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -1,39 +1,38 @@
 class Solution {
     public char processStr(String s, long k) {
-        int n = s.length();
-        long[] lens = new long[n];
         long len = 0;
 
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            if (c == '*')
-                len = Math.max(len - 1, 0L);
-            else if (c == '#')
-                len *= 2;
-            else if (c != '%')
-                len++;
-            
-            lens[i] = len;
+        // Step 1
+        for (char c : s.toCharArray()) {
+            if (c == '*') len = Math.max(0, len - 1);
+            else if (c == '#') len *= 2;
+            else if (c != '%') len++;
         }
 
         if (k >= len) return '.';
 
-        for (int i = n - 1; ; i--) {
+        // Step 2
+        for (int i = s.length() - 1; i >= 0; i--) {
             char c = s.charAt(i);
-            switch (c) {
-                case '*':
-                    break;
-                case '#':
-                    if (k >= lens[i] / 2)
-                        k -= lens[i] / 2;
-                    break;
-                case '%':
-                    k = lens[i] - 1 - k;
-                    break;
-                default:
-                    if (lens[i] == k + 1)
-                        return c;
+
+            if (c == '*') len++;
+
+            else if (c == '#') {
+                long half = len / 2;
+                if (k >= half) k -= half;
+                len = half;
+            }
+
+            else if (c == '%') {
+                k = len - 1 - k;
+            }
+
+            else {
+                if (k == len - 1) return c;
+                len--;
             }
         }
+
+        return '.';
     }
 }
